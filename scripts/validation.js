6fb4c64b4b4590a6d91b5aa7c5b3125b28f49070
Phil Peraza <phil.peraza@outlook.com>
2023-09-26 06:34:15 -0700

    fix/ const element object selector

--- scripts/validation.js
+++ scripts/validation.js
@@ -40,7 +40,7 @@ function toggleButtonState(inputEls, submitButton, {inactiveButtonClass}) {
 function setEventListeners(formEl, options) {
     const {inputSelector} = options;
     const inputEls = [...formEl.querySelectorAll(inputSelector)];
-    const submitButton = formEl.querySelector(".modal__button");
+    const submitButton = formEl.querySelector(options.submitButtonSelector);
 
     inputEls.forEach(inputEl => {
         inputEl.addEventListener("input", (e) => {
