d1d86d33d36b821d12da78370f35a6c22d51a50d
Phil Peraza <phil.peraza@outlook.com>
2023-11-28 00:52:08 -0800

    fix/ constants, html, and index.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,19 +1,40 @@
-class Api {
-    constructor(options) {
-      // constructor body
+// API Token: 60774818-2b72-4a3b-8d83-5b2abf9c8a5e
+
+/*export class Api {
+    constructor(config) {
+      this.url = config.baseUrl;
+      this.headers = config.headers;
     }
   
     getInitialCards() {
-      // ...
+      return fetch(`${this.url}/cards`, {
+        headers: {
+          authorization: "60774818-2b72-4a3b-8d83-5b2abf9c8a5e",
+        },
+      })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      });
     }
-  
-    // other methods for working with the API
+  }*/
+
+  export default class Api {
+    constructor(config) {
+      this.url = config.baseUrl;
+      this.headers = config.headers;
+    }
+    getInitialCards() {
+      return fetch("https://around-api.en.tripleten-services.com/v1", {
+        headers: {
+          authorization: "60774818-2b72-4a3b-8d83-5b2abf9c8a5e"
+        }
+      }).then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+    }
   }
-  
-  const api = new Api({
-    baseUrl: "https://around-api.en.tripleten-services.com/v1",
-    headers: {
-      authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
-      "Content-Type": "application/json"
-    }
-  });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,75 +1,16 @@
-export const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  },
-];
-
-export const cardData = {
-  name: "Yosemite Valley",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-};
-
-export const cardsWrap = document.querySelector(".cards__list");
-
-export const profileEditModal = document.querySelector("#profile-edit-modal");
-export const profileEditForm = document.forms["profile-edit-form"];
 export const profileEditOpenButton = document.querySelector(
-  "#profile-edit-button"
+  "#edit-profile-open-button"
 );
-export const profileEditCloseButton = profileEditModal.querySelector(
-  "#profile-close-modal-button"
+export const addCardOpenButton = document.querySelector(
+  "#add-card-open-button"
 );
-
-export const addCardModal = document.querySelector("#profile-add-card-modal");
-export const addCardForm = document.forms["add-card-form"];
-export const addCardOpenButton = document.querySelector(".profile__add-button");
-export const addCardCloseButton = addCardModal.querySelector(
-  "#add-card-close-button"
-);
-
-export const imageModal = document.querySelector("#popup-modal");
-export const imageModalPreview = document.querySelector(".modal__image_popup");
-export const previewModalCloseButton = document.querySelector(
-  "#popup-image-close-button"
-);
-export const modalCaption = document.querySelector(".modal__name_popup");
-
-export const profileTitle = document.querySelector(".profile__title");
-export const profileDescription = document.querySelector(
-  ".profile__description"
+export const editAvatarOpenButton = document.querySelector(
+  "#avatar-edit-button"
 );
 
 export const profileTitleInput = document.querySelector("#profile-title-input");
 export const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
-);
-
-export const newCardTitleInput = document.querySelector(
-  ".modal__input_type_title"
-);
-export const newCardLinkInput = document.querySelector(
-  ".modal__input_type_url"
 );
 
 export const config = {
